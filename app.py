5fce2ba31a0eaf0c74001dbdb2b62d16f7cc824a
Nikita <nik.kat.05@bk.ru>
2025-02-02 19:51:14 +0500

    добавлена реализация DTO

--- app.py
+++ app.py
@@ -15,18 +15,52 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+# DTO для информации о сервере
+class ServerInfoDTO:
+    def __init__(self, python_version):
+        self.python_version = python_version
+
+    def to_dict(self):
+        return {
+            "python_version": self.python_version
+        }
+
+# DTO для информации о клиенте
+class ClientInfoDTO:
+    def __init__(self, ip, user_agent):
+        self.ip = ip
+        self.user_agent = user_agent
+
+    def to_dict(self):
+        return {
+            "ip": self.ip,
+            "user_agent": self.user_agent
+        }
+
+# DTO для информации о базе данных
+class DatabaseInfoDTO:
+    def __init__(self, database_version):
+        self.database_version = database_version
+
+    def to_dict(self):
+        return {
+            "database_version": self.database_version
+        }
+
 # Роут для получения информации о сервере
 @app.route('/info/server', methods=['GET'])
 def server_info():
     python_version = platform.python_version()
-    return jsonify({'python_version': python_version})
+    dto = ServerInfoDTO(python_version=python_version)
+    return jsonify(dto.to_dict())
 
 # Роут для получения информации о клиенте
 @app.route('/info/client', methods=['GET'])
 def client_info():
     client_ip = request.remote_addr
     user_agent = request.headers.get('User-Agent')
-    return jsonify({'ip': client_ip, 'user_agent': user_agent})
+    dto = ClientInfoDTO(ip=client_ip, user_agent=user_agent)
+    return jsonify(dto.to_dict())
 
 # Роут для получения информации о базе данных
 @app.route('/info/database', methods=['GET'])
@@ -36,7 +70,8 @@ def database_info():
     cursor.execute("SELECT sqlite_version() AS version")
     db_info = cursor.fetchone()
     conn.close()
-    return jsonify({'database_version': db_info['version']})
+    dto = DatabaseInfoDTO(database_version=db_info['version'])
+    return jsonify(dto.to_dict())
 
 # Запуск приложения
 if __name__ == '__main__':
